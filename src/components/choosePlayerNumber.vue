3750bad91bc54d97a1b6667c937defc0b9591dc8
JELLON <1849930892@qq.com>
2022-07-12 00:42:36 +0800

    card

---
+++ src/components/choosePlayerNumber.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "choosePlayerNumber"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/popUp.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "popUp"
+}
+</script>
+
+<style scoped>
+
+</style>
